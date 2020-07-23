5f94d007d6401dcc54de3b76493d4852fee67dbd
Yuchong Pan <panyuchong@gmail.com>
2020-07-23 13:57:21 -0700

    Added single benchmark for profiling

---
+++ src/benchmarks/micro/runtime/Iterations/Iterations_SingleBenchmark.cs
@@ -0,0 +1,48 @@
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+
+namespace Iterations
+{
+    [BenchmarkCategory(Categories.Runtime)]
+    public class Iterations_SingleBenchmark
+    {
+        public const int MOD = 998244353;
+
+        [Params(16384 * 32768)]
+        public int N;
+
+        public static int execute(int num_iterations)
+        {
+            int i;
+            int a = 0, b = 1;
+
+            for (i = 0; i < num_iterations; i++)
+            {
+                update(ref a, ref b);
+            }
+
+            return a;
+        }
+
+        [MethodImplAttribute(MethodImplOptions.NoInlining)]
+        private static void update(ref int a, ref int b)
+        {
+            int c = a + b;
+
+            if (c >= MOD)
+            {
+                c -= MOD;
+            }
+
+            a = b;
+            b = c;
+        }
+
+        [Benchmark]
+        public void bench()
+        {
+            execute(N);
+        }
+    }
+}
