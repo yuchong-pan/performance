be6e084cd07598c9276baecf6abb91d0d78e734c
Yuchong Pan <panyuchong@gmail.com>
2020-07-29 16:34:47 -0700

    Added event sources to track the start and end of iterations

--- src/benchmarks/micro/runtime/Iterations/Iterations_SingleBenchmark.cs
+++ src/benchmarks/micro/runtime/Iterations/Iterations_SingleBenchmark.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Tracing;
 using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using MicroBenchmarks;
@@ -17,10 +18,14 @@ namespace Iterations
             int i;
             int a = 0, b = 1;
 
+            IterationsBenchmarkEventSource.Log.Startup();
+
             for (i = 0; i < num_iterations; i++)
             {
                 update(ref a, ref b);
             }
+
+            IterationsBenchmarkEventSource.Log.End();
 
             return a;
         }
@@ -43,6 +48,24 @@ namespace Iterations
         public void bench()
         {
             execute(N);
+        }
+    }
+
+    [EventSource(Name = "IterationsBenchmark")]
+    public class IterationsBenchmarkEventSource : EventSource
+    {
+        public static IterationsBenchmarkEventSource Log = new IterationsBenchmarkEventSource();
+
+        [Event(1, Message = "Starting up benchmark execution.")]
+        public void Startup()
+        {
+            WriteEvent(1);
+        }
+
+        [Event(2, Message = "Ended benchmark execution.")]
+        public void End()
+        {
+            WriteEvent(2);
         }
     }
 }
